90b14cc6548fdf13d0ec710bb53b623d0b6ff075
Anna Baiotto <anna.baiotto@gmail.com>
2020-05-09 15:41:48 +0200

    1er jet du Front Anna le samedi09

--- src/main/java/com/slowlife/controller/ParticulierController.java
+++ src/main/java/com/slowlife/controller/ParticulierController.java
@@ -3,6 +3,7 @@ package com.slowlife.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.slowlife.entity.Particulier;
 import com.slowlife.service.ParticulierService;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class ParticulierController {
 
