41a735a06a8db1a96f4103ea566e830523811df9
Anna Baiotto <anna.baiotto@gmail.com>
2020-05-12 11:24:53 +0200

    modification Anna mardi

--- src/main/java/com/slowlife/dao/ArticleDao.java
+++ src/main/java/com/slowlife/dao/ArticleDao.java
@@ -6,10 +6,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.slowlife.entity.Article;
+import com.slowlife.entity.Artisant;
+import com.slowlife.entity.Commercant;
+import com.slowlife.entity.Particulier;
+import com.slowlife.entity.Producteur;
 
 public interface ArticleDao extends JpaRepository<Article, Integer>{
 	public List<Article> findByNom(String nom);
 	
 	@Query("SELECT a FROM Article a WHERE a.nom = ?1 and a.producteur.ville = ?2")
 	public List<Article> findByNomAndVille(String nom,String ville);
+	
+	public List<Article> findByProducteur(Producteur p);
+	public List<Article> findByArtisant(Artisant a);
+	public List<Article> findByCommercant(Commercant c);
+	
 }
